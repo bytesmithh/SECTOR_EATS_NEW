49749ea72226f7a77f46fd68e2ff1ef50bd44ef6
Amritpal Singh <amritpalsinghs0945@gmail.com>
2025-04-26 20:48:25 +0530

    Update forms.py

--- FoodDelivery/RestaurantApp/forms.py
+++ FoodDelivery/RestaurantApp/forms.py
@@ -11,13 +11,12 @@ class RestaurantForm(forms.ModelForm):
             'closing_time': forms.TimeInput(attrs={'class': 'form-control', 'placeholder': 'HH:MM'}),
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'location': forms.TextInput(attrs={'class': 'form-control'}),
-            'cuisine_type': forms.TextInput(attrs={'class': 'form-control'}),
+            'cuisine_type': forms.Select(attrs={'class': 'form-control'}),
             'contact': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
             'delivery_available': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
-            'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
 
         exclude = ['admin']  
